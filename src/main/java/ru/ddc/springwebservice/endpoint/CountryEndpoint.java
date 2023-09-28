7d3ca726ab7feadf5223ca4b6be93a7a18cf03ff
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-28 22:54:11 +0300

    KTE_Labs :: добавлен SOAP Web Service по примеру

---
+++ src/main/java/ru/ddc/springwebservice/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package ru.ddc.springwebservice.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import ru.ddc.springwebservice.dto.GetCountryRequest;
+import ru.ddc.springwebservice.dto.GetCountryResponse;
+import ru.ddc.springwebservice.repository.CountryRepository;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://ddc.ru/springwebservice/dto";
+
+    private final CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}

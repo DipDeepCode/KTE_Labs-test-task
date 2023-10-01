34f20cf904a82776f08586afe3365a3f6b5e5f3d
Albina <albina.melsitova@mail.ru>
2023-10-01 22:05:44 +0300

    Delete files from server and adding method in class CountryRepo

--- src/main/java/ru/ddc/springwebservice/repository/CountryRepo.java
+++ src/main/java/ru/ddc/springwebservice/repository/CountryRepo.java
@@ -3,6 +3,9 @@ package ru.ddc.springwebservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ddc.springwebservice.models.CountryEntity;
 
+import java.util.Optional;
+
 public interface CountryRepo extends JpaRepository<CountryEntity, Long> {
+    public Optional<CountryEntity> findByName(String name);
 
 }
